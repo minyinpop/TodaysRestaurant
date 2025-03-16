4334c0f6bd49b6506b287dc0131c2000821d2187
minyinpop <minyinpop@gmail.com>
2025-03-16 17:52:39 +0800

    2025.03.16 17:52

--- Scripts/Player/PlayerItemDragSystem.cs
+++ Scripts/Player/PlayerItemDragSystem.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using Item;
 using Storage.Slot;
-using Storage.Slot.Category;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -24,23 +23,14 @@ namespace Player
         [Tooltip("儲物格的標籤，用於判斷滑鼠有沒有點擊到儲物格。"), SerializeField]
         private string storageSlotTag;
         
-        // 物品預覽儲物格的遊戲物件。
-        private GameObject _itemPreview;
+        // 用來判斷物品是否正在拖曳的狀態。
+        private bool _isDragging;
         
-        // 物品預覽儲物格的資料庫。
-        private StorageSlotData _itemPreviewSlotData;
+        // 物品拖曳預覽的快取資料。
+        private TempStorageSlotData _previewStorageSlotData;
         
-        // 儲物格的遊戲物件，用於獲取第一次點擊到的儲物格的所有訊息。
-        private GameObject _selectedSlot;
-        private GameObject _tempSelectedSlot;
-
-        // _selectedSlot 的介面資訊，用於快速讀取資料用。
-        private StorageSlotUI _selectedSlotUI;
-        private StorageSlotUI _tempSelectedSlotUI;
-        
-        // _selectedSlot 的儲物格的資料，用於快速讀取資料用。
-        private StorageSlotData _selectedSlotData;
-        private StorageSlotData _tempSelectedSlotData;
+        // 被點選的物品的快取資料。
+        private TempStorageSlotData _selectedStorageSlotData;
         
         // 輸入端。
         private InputMap _input;
@@ -62,8 +52,8 @@ namespace Player
         private void LateUpdate()
         {
             // 如果拖曳預覽不存在的話，就不執行。
-            if (_itemPreview is not null)
-                _itemPreview.transform.position = MousePos;
+            if (_previewStorageSlotData.slot is not null)
+                _previewStorageSlotData.slot.transform.position = MousePos;
         }
 
         private void OnDisable()
@@ -78,6 +68,45 @@ namespace Player
         private void OnLeftClickPerformed(InputAction.CallbackContext context)
         {
             // TODO: 玩家滑鼠的各種互動。
+            _selectedStorageSlotData = UpdateTempSlotData(false, MouseDetectedSlot());
+            
+            // 如果玩家沒有點擊到儲物格。
+            if (_selectedStorageSlotData.slot is null)
+            {
+            }
+            // 如果玩家點擊到儲物格。
+            else
+            {
+                _selectedStorageSlotData = UpdateTempSlotData(true, _selectedStorageSlotData.slot);
+                
+                // 玩家在拖曳物品時，點擊了儲物格。
+                if (_isDragging)
+                {
+                    // 玩家在拖曳物品的時候，點擊了空的儲物格。
+                    if (_selectedStorageSlotData.slotData.itemData is null)
+                    {
+                        print("物品被放入了空的儲物格");
+                    }
+                    // 玩家在拖曳物品的時候，點擊了有物品的儲物格。
+                    else
+                    {
+                        print("物品與該儲物格內的物品做邏輯比較");
+                    }
+                }
+                // 玩家在空手時，點擊了儲物格，就判斷是否要生成物品預覽。
+                else
+                {
+                    // 玩家在空手時，點擊了沒有儲存物品的儲物格，就直接清空暫存數據，並直接退出判斷。
+                    if (_selectedStorageSlotData.slotData.itemData is null)
+                        return;
+
+                    // 玩家在空手時，點擊了有儲存物品的儲物格，就生成物品拖曳預覽，並且更新其介面。
+                    _isDragging = true;
+                    
+                    _previewStorageSlotData = UpdateTempSlotData(true, Instantiate(itemDragPreviewPrefab, previewSpawnPoint));
+                    _previewStorageSlotData.slotUI.Refresh(_selectedStorageSlotData.slotData);
+                }
+            }
         }
 
         /// <summary>
@@ -127,18 +156,54 @@ namespace Player
         }
 
         /// <summary>
+        /// 用來更新儲物格的快取資料。
+        /// </summary>
+        /// <param name="fullData"> 是否只儲存儲物格的遊戲物件。 </param>>
+        /// <param name="slot"> 儲物格的遊戲物件。 </param>
+        /// <returns> 回傳薪的儲物格快取資料。 </returns>
+        private static TempStorageSlotData UpdateTempSlotData(bool fullData, GameObject slot)
+        {
+            if (fullData)
+            {
+                return new TempStorageSlotData
+                {
+                    slot = slot,
+                    slotUI = slot.GetComponent<StorageSlotUI>(),
+                    slotData = slot.GetComponent<StorageSlotUI>().SlotData()
+                };
+            }
+
+            return new TempStorageSlotData
+            {
+                slot = slot,
+                slotUI = null,
+                slotData = new StorageSlotData()
+            };
+        }
+
+        /// <summary>
         /// 重置所有的儲物格的暫存的資料。
         /// </summary>
-        private void Reset()
+        private void ClearTempSlotData()
         {
-            _selectedSlot = null;
-            _tempSelectedSlot = null;
-            
-            _selectedSlotUI = null;
-            _tempSelectedSlotUI = null;
-            
-            _selectedSlotData = new StorageSlotData();
-            _tempSelectedSlotData = new StorageSlotData();
+            _previewStorageSlotData = new TempStorageSlotData();
+            _selectedStorageSlotData = new TempStorageSlotData();
         }
+    }
+    
+    /// <summary>
+    /// 用來快速讀取儲物格的各項資料，只限於暫存專用。
+    /// </summary>
+    [System.Serializable]
+    public struct TempStorageSlotData
+    {
+        // 儲物格的遊戲物件。
+        public GameObject slot;
+        
+        // 儲物格的介面資料。
+        public StorageSlotUI slotUI;
+        
+        // 儲物格的儲物資料。
+        public StorageSlotData slotData;
     }
 }
