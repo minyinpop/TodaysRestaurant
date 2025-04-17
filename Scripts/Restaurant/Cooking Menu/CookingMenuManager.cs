e34ae64aedd1486468b403504a2babbdb33b4ea4
minyinpop <minyinpop@gmail.com>
2025-04-17 11:58:55 +0800

    2025.04.17 11:58

---
+++ Scripts/Restaurant/Cooking Menu/CookingMenuManager.cs
@@ -0,0 +1,11 @@
+using Database.Restaurant.Chosen;
+using UnityEngine;
+
+namespace Restaurant.Cooking_Menu
+{
+    public class CookingMenuManager : MonoBehaviour
+    {
+        //
+        private ChosenMealsTypeSO _chosenMealsTypeSO;
+    }
+}
--- Scripts/Restaurant/Player/PlayerManager.cs
+++ Scripts/Restaurant/Player/PlayerManager.cs
@@ -8,6 +8,7 @@ namespace Restaurant.Player
     
     [RequireComponent(typeof(PlayerAnimator))]
     [RequireComponent(typeof(PlayerMove))]
+    [RequireComponent(typeof(PlayerInteractionDetector))]
     public class PlayerManager : MonoBehaviour
     {
         
