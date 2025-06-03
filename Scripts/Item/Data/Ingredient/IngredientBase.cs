1e7068505d1a574ae259eb27bb7ca499e591b459
minyinpop <minyinpop@gmail.com>
2025-06-03 10:56:46 +0800

    2025.06.03 10:56

--- Scripts/Item/Data/Ingredient/IngredientBase.cs
+++ Scripts/Item/Data/Ingredient/IngredientBase.cs
@@ -6,6 +6,21 @@ namespace Item.Data.Ingredient
     [CreateAssetMenu(menuName = "Today's Restaurant/Item/Ingredient", fileName = "New Data", order = 1)]
     internal class IngredientBase : ItemBase
     {
-        
+        [field: SerializeField] internal InfoSettings InfoSettings { get; private set; }
+        [field: SerializeField] internal StackSettings StackSettings { get; private set; }
+    }
+
+    [System.Serializable]
+    internal class InfoSettings
+    {
+        [field: SerializeField] internal string Name { get; private set; }
+        [field: SerializeField] internal Sprite Sprite { get; private set; }
+    }
+    
+    [System.Serializable]
+    internal class StackSettings
+    {
+        [field: SerializeField] internal bool CanStack { get; private set; }
+        [field: Range(1, 99), SerializeField] internal int MaxStack { get; private set; }
     }
 }
