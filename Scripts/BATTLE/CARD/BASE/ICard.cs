c81e3d089d25c2efcada9f5efff7495c012efbd8
minyinpop <minyinpop@gmail.com>
2025-06-09 15:28:52 +0800

    2025.06.09 15:28

--- Scripts/BATTLE/CARD/BASE/ICard.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine.EventSystems;
-
-namespace BATTLE.CARD.BASE
-{
-    internal interface ICard : IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
-    {
-        public void OnUse();
-    }
-}
