8886ab761ae7f4d958404fe368b952354a4f2050
minyinpop <minyinpop@gmail.com>
2025-06-09 23:17:15 +0800

    2025.06.09 23:17

--- Scripts/BATTLE/CARD/MANAGER/CardBase.cs
+++ Scripts/BATTLE/CARD/MANAGER/CardBase.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -5,12 +6,95 @@ namespace BATTLE.CARD.MANAGER
 {
     internal abstract class CardBase : MonoBehaviour, ICard
     {
-        public abstract void OnPointerEnter(PointerEventData eventData);
-        public abstract void OnPointerExit(PointerEventData eventData);
-        public abstract void OnPointerDown(PointerEventData eventData);
-        public abstract void OnPointerUp(PointerEventData eventData);
-        public abstract void OnBeginDrag(PointerEventData eventData);
-        public abstract void OnDrag(PointerEventData eventData);
-        public abstract void OnEndDrag(PointerEventData eventData);
+        private bool Selected { get; set; }
+        
+        private Sequence ShakePos { get; set; }
+        private Sequence ShakeRot { get; set; }
+        
+        private Tween PosTween { get; set; }
+        private Tween RotTween { get; set; }
+        private Tween ScaleTween { get; set; }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            ScaleTween = transform
+                .DOScale(
+                    endValue: Vector3.one * 1.2f,
+                    duration:.2f
+                    )
+                .SetEase(
+                    ease: Ease.OutSine
+                    );
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            ScaleTween = transform
+                .DOScale(
+                    endValue: Vector3.one,
+                    duration: .2f
+                    )
+                .SetEase(
+                    ease: Ease.OutSine
+                    );
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Selected = !Selected;
+            
+            ShakePos = DOTween.Sequence();
+            ShakeRot = DOTween.Sequence();
+
+            ShakePos
+                .Append(PosTween = transform
+                    .DOShakePosition(
+                        duration: .2f,
+                        strength: 10,
+                        vibrato: 10,
+                        randomness: 90,
+                        snapping: false,
+                        fadeOut: true,
+                        randomnessMode: ShakeRandomnessMode.Harmonic
+                    )
+                    .SetEase(
+                        ease: Ease.OutSine
+                    )
+                )
+                .Join(
+                    PosTween = transform
+                    .DOMoveY(
+                        endValue: Selected ? transform.parent.position.y + 100 : transform.parent.position.y,
+                        duration: .5f
+                        )
+                    .SetEase(
+                        ease: Ease.OutSine
+                        )
+                );
+
+            ShakeRot
+                .Append(RotTween = transform
+                    .DOShakeRotation(
+                        duration: .2f,
+                        strength: Vector3.forward * 10,
+                        vibrato: 10,
+                        randomness: 90,
+                        fadeOut: true,
+                        randomnessMode: ShakeRandomnessMode.Harmonic
+                        )
+                    .SetEase(
+                        ease: Ease.OutSine
+                        )
+                )
+                .Append(RotTween = transform
+                    .DORotate(
+                        endValue: Vector3.zero,
+                        duration: .2f
+                        )
+                    .SetEase(
+                        ease: Ease.OutSine
+                        )
+                );
+        }
     }
 }
--- Scripts/BATTLE/CARD/MANAGER/ICard.cs
+++ Scripts/BATTLE/CARD/MANAGER/ICard.cs
@@ -2,7 +2,7 @@ using UnityEngine.EventSystems;
 
 namespace BATTLE.CARD.MANAGER
 {
-    internal interface ICard : IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+    internal interface ICard : IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
     {
         
     }
