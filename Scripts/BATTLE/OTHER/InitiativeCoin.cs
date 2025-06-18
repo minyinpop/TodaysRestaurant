e06977ef9d79dbc3aa8a0317b348197a25399809
minyinpop <minyinpop@gmail.com>
2025-06-18 11:59:01 +0800

    2025.06.18 11:59

--- Scripts/BATTLE/OTHER/InitiativeCoin.cs
+++ Scripts/BATTLE/OTHER/InitiativeCoin.cs
@@ -12,31 +12,64 @@ namespace BATTLE.OTHER
         // State
         private bool CanClick { get; set; }
 
+        public static event System.Action FinishFlipEvent;
+
         private void Awake()
         {
             RectTransform = GetComponent<RectTransform>();
         }
-
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parent"></param>
         public void SlideInScreen(RectTransform parent)
         {
             RectTransform.SetParent(parent);
 
             RectTransform
                 .DOAnchorPos(Vector2.zero, .75f, true)
+                .SetEase(Ease.OutBack)
                 .OnComplete(() => { CanClick = true; });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parent"></param>
+        public void SlideToMiddle(RectTransform parent)
+        {
+            // TODO 解決更改父物件會導致位移的問題
+            var lastRect = RectTransform.anchoredPosition;
+            
+            RectTransform.SetParent(parent);
+            
+            RectTransform.anchoredPosition = lastRect;
+            
+            DOTween.Sequence()
+                .Append(RectTransform
+                    .DOAnchorPos(Vector2.zero, .3f, true)
+                    .SetEase(Ease.OutQuad))
+                .Append(RectTransform
+                    .DOScale(Vector2.one * 2, .3f)
+                    .SetEase(Ease.InOutBack));
         }
 
         #region Pointer Events
         public void OnPointerEnter(PointerEventData eventData)
         {
             if (!CanClick) return;
-            RectTransform.DOScale(Vector2.one * 2, .25f);
+            RectTransform
+                .DOScale(Vector2.one * 2, .25f)
+                .SetEase(Ease.OutQuart);
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
             if (!CanClick) return;
-            RectTransform.DOScale(Vector2.one, .25f);
+            RectTransform
+                .DOScale(Vector2.one, .25f)
+                .SetEase(Ease.OutQuart);
         }
 
         public void OnPointerClick(PointerEventData eventData)
@@ -44,8 +77,35 @@ namespace BATTLE.OTHER
             if (!CanClick) return;
             CanClick = false;
             
-            var randomDistanceX = Screen.width * Random.Range(-.25f, .25f);
-            var randomDistanceY = Screen.height * Random.Range(.1f, .5f);
+            DOTween.Sequence()
+                .Append(RectTransform
+                    .DOScale(Vector2.one, 1)
+                    .SetEase(Ease.InBack))
+                .JoinCallback(() =>
+                {
+                    var randomDistanceX = Screen.width * Random.Range(-.25f, .25f);
+                    var randomDistanceY = Screen.height * Random.Range(.1f, .5f);
+                    
+                    RectTransform
+                        .DOAnchorPos(new Vector2(randomDistanceX, randomDistanceY), 2, true)
+                        .SetEase(Ease.OutQuad);
+                })
+                .JoinCallback(() =>
+                {
+                    const int rotateAngleY = 180;
+                    var rotateTimesY = Random.Range(8, 16);
+                    var randomRotateY = rotateAngleY * rotateTimesY;
+
+                    var rotateAngleZ = Random.Range(0, 361);
+                    var rotateTimesZ = Random.Range(8, 16);
+                    var randomRotateZ = rotateAngleZ * rotateTimesZ;
+                    
+                    RectTransform
+                        .DORotate(new Vector3(0, randomRotateY, randomRotateZ), 2, RotateMode.FastBeyond360)
+                        .SetEase(Ease.OutCubic);
+                })
+                .AppendInterval(1)
+                .OnComplete(() => { FinishFlipEvent?.Invoke(); });
         }
         #endregion
     }
--- Scripts/BATTLE/SYSTEM/MAIN/TYPE/InitiativeSelectionSystem.cs
+++ Scripts/BATTLE/SYSTEM/MAIN/TYPE/InitiativeSelectionSystem.cs
@@ -9,19 +9,35 @@ namespace BATTLE.SYSTEM.MAIN.TYPE
         [field: Header("Initiative Coin")]
         [field: SerializeField] private RectTransform SpawnParent { get; set; }
         [field: SerializeField] private RectTransform ReadyParent { get; set; }
+        [field: SerializeField] private RectTransform ShowParent { get; set; }
         [field: SerializeField] private GameObject InitiativeCoinPrefab { get; set; }
         
-        private GameObject InitiativeCoin { get; set; }
+        private GameObject InitiativeCoinObj { get; set; }
         private RectTransform InitiativeCoinRect { get; set; }
         private InitiativeCoin InitiativeCoinScript { get; set; }
 
+        private void OnEnable()
+        {
+            InitiativeCoin.FinishFlipEvent += FinishFlip;
+        }
+        
+        private void OnDisable()
+        {
+            InitiativeCoin.FinishFlipEvent -= FinishFlip;
+        }
+
         public void Init()
         {
-            InitiativeCoin = Instantiate(InitiativeCoinPrefab, SpawnParent);
-            InitiativeCoinRect = InitiativeCoin.GetComponent<RectTransform>();
-            InitiativeCoinScript = InitiativeCoin.GetComponent<InitiativeCoin>();
+            InitiativeCoinObj = Instantiate(InitiativeCoinPrefab, SpawnParent);
+            InitiativeCoinRect = InitiativeCoinObj.GetComponent<RectTransform>();
+            InitiativeCoinScript = InitiativeCoinObj.GetComponent<InitiativeCoin>();
             
             InitiativeCoinScript.SlideInScreen(ReadyParent);
+        }
+
+        private void FinishFlip()
+        {
+            InitiativeCoinScript.SlideToMiddle(ShowParent);
         }
     }
 }
