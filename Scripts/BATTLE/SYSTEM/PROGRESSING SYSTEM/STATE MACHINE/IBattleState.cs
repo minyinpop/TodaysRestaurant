5add173dec290abdb683c4e11363fe3845e8f3f2
minyinpop <minyinpop@gmail.com>
2025-07-04 01:59:54 +0800

    2025.07.04 01:59

--- Scripts/BATTLE/SYSTEM/PROGRESSING SYSTEM/STATE MACHINE/IBattleState.cs
+++
@@ -1,8 +0,0 @@
-namespace BATTLE.SYSTEM.PROGRESSING_SYSTEM.STATE_MACHINE
-{
-    internal interface IBattleState
-    {
-        public void Enter();
-        public void Exit();
-    }
-}
---
+++ Scripts/DATA/CARD/DefenseCardSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace DATA.CARD
+{
+    [CreateAssetMenu(menuName = "Today's Restaurant/Battle/Defense Card Data", fileName = "New Data")]
+    internal class DefenseCardSO : ScriptableObject
+    {
+        
+    }
+}
