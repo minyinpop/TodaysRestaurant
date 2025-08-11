9c7f42e97de8d5f8d986fd486063bcfb17bc8aeb
minyinpop <minyinpop@gmail.com>
2025-08-11 21:41:40 +0800

    2025.08.11 21:41
    硬幣操作模型完成，開始撰寫正式版。

--- Scripts/BATTLE/SYSTEM/INITIATIVE/STATE MACHINE/IInitiativeState.cs
+++
@@ -1,7 +0,0 @@
-namespace BATTLE.SYSTEM.INITIATIVE.STATE_MACHINE
-{
-    internal interface IInitiativeState
-    {
-        public void Enter(InitiativeSystem system);
-    }
-}
--- Scripts/BATTLE/SYSTEM/PROCESS/STATE MACHINE/IProcessState.cs
+++
@@ -1,7 +0,0 @@
-namespace BATTLE.SYSTEM.PROCESS.STATE_MACHINE
-{
-    internal interface IProcessState
-    {
-        public void Enter(ProcessSystem system);
-    }
-}
