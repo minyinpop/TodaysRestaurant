9218b464a0a49131e8d0b15844c681242dc00a5c
minyinpop <minyinpop@gmail.com>
2025-08-14 00:56:04 +0800

    2025.08.14 00:55
    把各部系統改成模塊化，好方便未來重複利用。

--- Scripts/BATTLE/PROCESS SYSTEM/STATE MACHINE/IBattleProcessState.cs
+++
@@ -1,8 +0,0 @@
-namespace BATTLE.PROCESS_SYSTEM.STATE_MACHINE
-{
-    internal interface IBattleProcessState
-    {
-        public void OnEnter(BattleProcessSystem system);
-        public void OnExit();
-    }
-}
