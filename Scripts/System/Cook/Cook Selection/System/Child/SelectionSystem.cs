4c0fd710b0d006cc28b07ab764fb28800802d70a
minyinpop <minyinpop@gmail.com>
2025-10-05 20:38:18 +0800

    2025.10.05 20:38

---
+++ Scripts/System/Cook/Cook Selection/System/Child/SelectionSystem.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace System.Cook.Cook_Selection.System.Child
+{
+    internal sealed class SelectionSystem : MonoBehaviour
+    {
+    }
+}
---
+++ Scripts/System/Cook/Cook Selection/System/Main/CookSelectionSystem.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace System.Cook.Cook_Selection.System.Main
+{
+    internal sealed class CookSelectionSystem : MonoBehaviour
+    {
+    }
+}
