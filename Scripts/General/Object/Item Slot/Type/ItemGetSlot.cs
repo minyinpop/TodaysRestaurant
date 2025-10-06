d666e1cd3e63eca87c481108e4cc413236dc1c1f
minyinpop <minyinpop@gmail.com>
2025-10-06 21:42:59 +0800

    2025.10.06 21:42

--- Scripts/General/Object/Item Slot/Type/ItemGetSlot.cs
+++ Scripts/General/Object/Item Slot/Type/ItemGetSlot.cs
@@ -2,7 +2,6 @@ using System;
 using System.General;
 using Data.Animation.DOTween.Basic;
 using Data.Item.Base;
-using DG.Tweening;
 using General.Object.Item_Slot.Base;
 using UnityEngine;
 using UnityEngine.UI;
@@ -18,21 +17,24 @@ namespace General.Object.Item_Slot.Type
         [field: Header("Child System")]
         [field: SerializeField] private DoAnimation DoAnimation;
         
-        [field: Header("State")]
-        [field: SerializeField] private bool Interactable;
+        [field: Header("Animation Settings")]
+        [field: SerializeField] private DoScale ScaleUpSettings;
+        [field: SerializeField] private DoScale ScaleDownSettings;
 
         private ItemSO ItemData;
+
+        private bool Interactable = true;
         
         protected override void OnPointerEnter()
         {
             if (!Interactable) return;
-            DoAnimation.DoScale(BackgroundRect, new DoScale(Vector2.one * 1.2f, .2f, Ease.OutExpo));
+            DoAnimation.DoScale(BackgroundRect, ScaleUpSettings);
         }
 
         protected override void OnPointerExit()
         {
             if (!Interactable) return;
-            DoAnimation.DoScale(BackgroundRect, new DoScale(Vector2.one, .2f, Ease.OutExpo));
+            DoAnimation.DoScale(BackgroundRect, ScaleDownSettings);
         }
         
         public override void Add(ItemSO item, Action onComplete)
@@ -43,7 +45,7 @@ namespace General.Object.Item_Slot.Type
             ItemImage.sprite = sprite;
             ItemImage.gameObject.SetActive(true);
             BackgroundRect.localScale = Vector2.one * 1.25f;
-            DoAnimation.DoScale(BackgroundRect, new DoScale(Vector2.one, .5f, Ease.OutBounce),
+            DoAnimation.DoScale(BackgroundRect, ScaleDownSettings,
                 onComplete: () =>
                 {
                     Interactable = true;
